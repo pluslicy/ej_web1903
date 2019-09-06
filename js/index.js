3c47db93c625f806603c89100838292052a616e8
plusyuyu <532323119@qq.com>
2019-09-06 10:37:22 +0800

    添加了导航功能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 $(function(){
-	$("#wrapper").load("./pages/customer.html")
+	// 为导航添加事件
+	$("ul.left_nav").on("click","li",function(){
+		var url = $(this).attr("url");
+		$("#wrapper").load(url);
+	})
 });
