17bf53f183afa5303d0a78c5df04a2efb2461749
plusyuyu <532323119@qq.com>
2019-09-06 10:44:14 +0800

    优化了导航功能

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ $(function(){
 	// 为导航添加事件
 	$("ul.left_nav").on("click","li",function(){
 		var url = $(this).attr("url");
+		$("ul.left_nav > li").removeClass("current");
+		$(this).addClass("current");
 		$("#wrapper").load(url);
 	})
+
+	// 模拟点击第一个li
+	$("ul.left_nav > li:first-child").trigger("click");
+	
 });
